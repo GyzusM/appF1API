8c4650d487deec0e3f0e2a24b7269c90025e835c
Jesus Moran <chujuegos@gmail.com>
2024-07-25 23:12:44 -0600

    Calendario de Carreras Corregido

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -17,6 +17,16 @@ function Calendar() {
     return  diaInicio + ' ' + meses[mesInicio-1] + ' - ' + diaFin + ' ' + meses[mesFin-1]
   }
 
+  function fechaInicio(fecha, tipo){
+    let arrayFecha = fecha.split('-')
+
+    let dia = arrayFecha[2]
+    let mes = arrayFecha[1]
+
+    const meses = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic' ]
+  
+    return tipo ? meses [mes-1] : dia
+  }
 
   return (
     <>
@@ -25,8 +35,8 @@ function Calendar() {
       {data?.map((gp) => (
         <div key={gp.Circuit.circuitId} className='dateCell'>
           <div className='cellContent'>
-            <h2 className='font-[1000] text-4xl'>FEB</h2>
-            <h3 className='font-[1000] text-4xl'>29</h3>
+            <h2 className='font-[1000] text-4xl'>{ fechaInicio(gp.FirstPractice.date, true) }</h2>
+            <h3 className='font-[1000] text-4xl'>{ fechaInicio(gp.FirstPractice.date, false) }</h3>
 
             <img src={`../images/flags/${gp.Circuit.Location.country}.png`} alt="" />
             <p className='text-[20px] font-[1000] text-red-f1 mt-[15px]'>{gp.Circuit.Location.country}</p>
